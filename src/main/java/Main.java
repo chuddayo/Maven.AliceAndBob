c286c780ae78ce399c7c46b5a28c9eedbb8ec1ef
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 13:50:13 -0400

    greeted everyone, but hello again to alice and bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,16 @@ public class Main {
     public static void main(String[] args ){
         String userName;
 
-        System.out.print("What's your name? ");
+        System.out.print("What's your first name? ");
         Scanner in = new Scanner(System.in);
-        userName = in.nextLine();
+        userName = in.next(); // only gets first word
 
-        System.out.println("Hello " + userName);
+        userName = userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
+        if (userName.toLowerCase().equals("alice") || userName.toLowerCase().equals("bob")) {
+            System.out.println("Howdy " + userName + ". Good to see you again.");
+        }
+        else {
+            System.out.println("Pleased to meet you, " + userName + ".");
+        }
     }
 }
