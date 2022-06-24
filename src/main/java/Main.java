5d898360033efe67f16a5ace831e711acd400575
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 13:30:08 -0400

    setup scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
     public static void main(String[] args ){
+        String userName;
 
+        System.out.print("What's your name? ");
+        Scanner in = new Scanner(System.in);
+        userName = in.nextLine();
+
+        System.out.println("Hello " + userName);
     }
 }
